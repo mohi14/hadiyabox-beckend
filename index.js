11db11c7007cc62521b745117cea8a44fe9e62c7
delower-hasan <dhhasansaha11@gmail.com>
2023-02-09 18:14:25 +0600

    mongodb connected with server

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors')
+const {MongoClient, ServerApiVersion}= require('mongodb')
 const jwt = require('jsonwebtoken')
 require('dotenv').config()
 
@@ -9,6 +10,33 @@ const app = express()
 
 app.use(express.json())
 app.use(cors())
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3dkasq3.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run(){
+    try{
+        const laptopsCollection = client.db('coomercio').collection('laptops');
+
+
+
+
+        app.get('/laptops', async(req,res)=>{
+            const query ={};
+            const laptops = await laptopsCollection.find(query).toArray();
+            res.send(laptops)
+        })
+
+    }
+    finally{
+
+    }
+}
+
+run().catch(error=>console.error(error))
+
 
 app.get('/',(req,res)=>{
     res.send('server is running')
