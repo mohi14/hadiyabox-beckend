1da8818baa71b4dd69a240fa192f4794973fa5ca
MamtazDev <akter.mstmamtaz@gmail.com>
2023-02-28 19:28:43 +0600

    ticket delet and get method done

--- controller/ticketController.js
+++ controller/ticketController.js
@@ -19,6 +19,36 @@ const addTicket = async (req, res) => {
   //   console.log(req.body, "hello");
 };
 
+const deleteTicket = async (req, res) => {
+  //   const { wallet } = req.body;
+  Ticket.deleteOne({ _id: req.params.id }, (error) => {
+    if (error) {
+      res.status(500).send({
+        message: error.message,
+        status: 500,
+      });
+    } else {
+      res.status(200).send({
+        message: "Ticket Deleted Successfully",
+        status: 200,
+      });
+    }
+  });
+};
+
+const getTicket = async (req, res) => {
+  try {
+    const tickets = await Ticket.find({}).sort({ _id: -1 });
+    res.send(tickets);
+  } catch (err) {
+    res.status(500).send({
+      message: err.message,
+    });
+  }
+};
+
 module.exports = {
   addTicket,
+  deleteTicket,
+  getTicket,
 };
--- routes/ticketRoutes.js
+++ routes/ticketRoutes.js
@@ -1,12 +1,20 @@
 const express = require("express");
+const { isAdmin } = require("../config/auth");
 const router = express.Router();
 
-const { addTicket } = require("../controller/ticketController");
+const {
+  addTicket,
+  deleteTicket,
+  getTicket,
+} = require("../controller/ticketController");
 
 // add ticket
 router.post("/add", addTicket);
 
 // delete ticket
-router.post("/remove/:id");
+router.post("/remove/:id", isAdmin, deleteTicket);
+
+// get ticket
+router.get("/", getTicket);
 
 module.exports = router;
